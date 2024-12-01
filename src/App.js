b2574db7448fa5a81510b56d74390ad062fd2a19
abdelghani bahri <bahri.ghanou@gmail.com>
2024-12-01 16:19:38 +0100

    add google analytics

--- src/App.js
+++ src/App.js
@@ -6,8 +6,12 @@ import NavBar from "./components/NavBar";
 import Skills from "./components/Skills"
 import Projects from  "./components/Projects"
 import Contact from "./components/Contact";
+import ReactGA from "react-ga4";
 
 function App() {
+  const measurID= "G-HWZ4Q0NM9Z"
+  ReactGA.initialize(measurID);
+  ReactGA.send({ hitType: "pageview", page: "/"});
   return (
     <main className="bg-black">
       <header className="">
